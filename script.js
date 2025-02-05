fffabfcf01312c5ef98c2b8f30a20e4936508b65
Antonio Millán <amilbar951@g.educaand.es>
2025-02-05 18:10:21 +0100

    diseño inicio

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
     const btnMostrar = document.getElementById("btnMostrar");
-    const contenido = document.querySelector(".contenido");
-    const mensaje = document.querySelector(".mensaje");
 
     btnMostrar.addEventListener("click", function () {
-        contenido.style.display = "none";
-        mensaje.style.display = "block";
-        document.body.style.overflow = "auto";
+        // Al hacer clic en el botón, se hace scroll hasta la siguiente sección
+        document.querySelector(".seccion").scrollIntoView({
+            behavior: "smooth"
+        });
     });
-};
+});
